c4eea4beffabb668a910d695498f57dc0ef29ea9
Muhammad Anas <anastts.pk@gmail.com>
2012-12-26 17:15:54 +0500

    Create empty netbeans project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Anas
+ *
+ * Created on December 26, 2012, 5:13 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
